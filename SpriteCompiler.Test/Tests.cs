943eb36acbd2493d6a8a7055989403ad2fb2ccb3
Lucas Scharenbroich <lscharen@prowestgis.com>
2016-11-26 23:41:33 -0600

    Minimal test to ensure that the simplest case is working

---
+++ SpriteCompiler.Test/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpriteCompiler.Problem;
+using SpriteCompiler.AI;
+
+namespace SpriteCompiler.Test
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestEmptySprite()
+        {
+            // Arrange
+            var problem = SpriteGeneratorSearchProblem.CreateSearchProblem();
+            var search = SpriteGeneratorSearchProblem.Create();
+
+            // Act : solve the problem
+            var solution = search.Search(problem, new SpriteGeneratorState());
+
+            // Assert : The initial state IS the goal state
+            Assert.AreEqual(1, solution.Count());
+        }
+    }
+}
